83697726ba33531ffd868661cf6bf51129af8429
GABRIEL LOPES <frangabriel566@icloud.com>
2026-07-13 11:45:18 -0300

    auto: update

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -5,7 +5,7 @@ import { usePathname, useRouter } from 'next/navigation'
 import {
   Crown, LayoutDashboard, Package, Users, ShoppingCart,
   LogOut, ChevronRight, BarChart3, DollarSign, Layers,
-  UserCog, Sun, Moon, X, Wallet, Cake,
+  UserCog, Sun, Moon, X, Cake,
 } from 'lucide-react'
 import { cn } from '@/lib/utils'
 import { useState } from 'react'
